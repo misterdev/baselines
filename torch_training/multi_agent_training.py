cd59797be7fd7b6245432e9aa7411f161ceae6a3
u214892 <u214892@sbb.ch>
2019-07-10 14:06:56 +0200

    #42 run baselines in ci

--- torch_training/multi_agent_training.py
+++ torch_training/multi_agent_training.py
@@ -6,7 +6,6 @@ from collections import deque
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
-from dueling_double_dqn import Agent
 from importlib_resources import path
 
 import torch_training.Nets
@@ -15,7 +14,10 @@ from flatland.envs.observations import TreeObsForRailEnv
 from flatland.envs.predictions import ShortestPathPredictorForRailEnv
 from flatland.envs.rail_env import RailEnv
 from flatland.utils.rendertools import RenderTool
+from torch_training.dueling_double_dqn import Agent
 from utils.observation_utils import norm_obs_clip, split_tree
+
+print("multi_agent_trainging.py (1)")
 
 
 def main(argv):
@@ -219,6 +221,10 @@ def main(argv):
     plt.show()
 
 
+print("multi_agent_trainging.py (2)")
+
 if __name__ == '__main__':
     print("main")
     main(sys.argv[1:])
+
+print("multi_agent_trainging.py (3)")
